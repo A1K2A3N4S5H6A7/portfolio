736bad87b53a7310a308c302dc85f04f57f10c9f
Akansha Sharma <146797007+A1K2A3N4S5H6A7@users.noreply.github.com>
2026-07-05 16:56:14 +0530

    Initial Portfolio Website

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Portfolio Loaded Successfully");
+
+
+// Smooth Scroll Effect
+
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        const id = this.getAttribute("href");
+
+        document.querySelector(id).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
